00fd1bf2d2bba11b18559370a4609300ad509b65
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2021-10-15 10:36:20 -0300

    Initial Commit

---
+++ SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Lucas Martins Figueiredo on 15/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
